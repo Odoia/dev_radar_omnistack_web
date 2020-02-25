1ee200f6a7bb1839c1371dba1d77c16e0b1754a3
Tiago Henrique <tiagohenriqueodoia@gmail.com>
2020-02-25 09:46:46 -0300

    develop Add a web in reactnative
    - connect frontend in backend
    
    Signed-off-by: Tiago Henrique <tiagohenriqueodoia@gmail.com>

---
+++ src/App.js
@@ -0,0 +1,138 @@
+import React, { useState, useEffect } from 'react';
+import api from './services/api'
+
+import './global.css';
+import './App.css';
+import './Sidebar.css';
+import './Main.css';
+
+function App() {
+  const [devs, setDevs] = useState([]);
+
+  const [github_username, setGithubUsername] = useState('');
+  const [techs, setTechs] = useState('');
+  const [latitude, setLatitude] = useState('');
+  const [longitude, setLongitude] = useState('');
+
+  useEffect(() => {
+    navigator.geolocation.getCurrentPosition(
+      (position) => {
+        const { latitude, longitude } = position.coords;
+
+        setLatitude(latitude);
+        setLongitude(longitude);
+      },
+      (err) => {
+        console.log(err);
+      },
+      {
+        timeout: 30000,
+      }
+    )
+  },[]);
+
+  useEffect(() => {
+    async function loadDevs() {
+      const response = await api.get('/devs');
+      
+      setDevs(response.data);
+    }
+
+    loadDevs();
+  },[]);
+
+  async function handleAddDev(e) {
+    e.preventDefault();
+
+    const response = await api.post('/devs', {
+      github_username,
+      techs,
+      latitude,
+      longitude,
+    })
+
+    setGithubUsername('');
+    setTechs('');
+
+    setDevs([...devs, response.data]);
+  }
+
+  return (
+    <div id="app">
+      <aside>
+        <strong>Register </strong>
+        <form onSubmit={handleAddDev}>
+          <div className="input-block">
+            <label htmlFor="github_username"> github user </label>
+            <input
+              name="github_username"
+              id="github_username"
+              value={github_username}
+              onChange={e => setGithubUsername(e.target.value)}
+              required
+            />
+          </div>
+
+          <div className="input-block">
+            <label htmlFor="techs"> technologies </label>
+            <input
+              name="techs"
+              id="techs"
+              value={techs}
+              onChange={e => setTechs(e.target.value)}
+              required
+            />
+          </div>
+
+          <div className="input-group">
+            <div className="input-block">
+              <label htmlFor="latitude"> latitude </label>
+              <input 
+                type="number"
+                name="latitude"
+                id="latitude"
+                value={latitude}
+                onChange={e => setLatitude(e.target.value)}
+                required
+              />
+            </div>
+
+            <div className="input-block">
+              <label htmlFor="longitude"> longitude </label>
+              <input 
+                type="number"
+                name="longitude"
+                id="longitude"
+                value={longitude}
+                onChange={e => setLongitude(e.target.value)}
+                required
+              />
+            </div>
+          </div>
+
+          <button type="submit"> Send </button>
+        </form>
+      </aside>
+      
+      <main>
+        <ul>
+          {devs.map(dev => (
+          <li key={dev._id} className="dev-item">
+            <header>
+              <img src={dev.avatar_url} alt={dev.name} />
+                <div className="user-info">
+                  <strong>{dev.name}</strong>
+                  <span>{dev.techs.join(', ')}</span>
+                </div>
+              </header>
+              <p>{dev.bio}</p>
+              <a href={`https://github.com/${dev.github_username}`}>Acessar perfil do github</a>
+            </li>
+          ))}
+        </ul>
+      </main>
+    </div>
+  );
+}
+
+export default App;
